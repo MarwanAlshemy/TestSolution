939feb524d4283856f1414abefa269cd7a21fee1
Marwan Alshemy <MarwanAlshemy@hotmail.com>
2023-02-13 00:12:36 +0300

    done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,9 +11,34 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // Ahmed Ismail
-            Console.WriteLine(SayHello());
-            Console.WriteLine(SayHello("Ahmed Ismaeil"));
-          
+            //Console.WriteLine(SayHello());
+            //Console.WriteLine(SayHello("Ahmed Ismaeil"));
+
+            //string myname = SayHello();
+            //Console.WriteLine(SayHello());
+            //string mstring = SayHello("Marwan");
+            //Console.WriteLine(mstring);
+
+            //string fullName = SayHello("Marwan", "Alshemy");
+            //Console.WriteLine(fullName);
+            //Console.WriteLine(SayHello("Ahmed", "Ismaeil"));
+
+            //Employee employee = new Employee();
+            //employee.ID = 1;
+            //employee.FullName = "Mahmoud";
+
+            //GetDetails(employee);
+            //Console.WriteLine(GetDetails(employee)); //ConsoleApp1.Employee
+
+            //Console.WriteLine(SayHello(employee)); //Mahmoud
+
+
+
+            Employee emp = new Employee();
+            emp.ID = 1;
+            emp.FullName = "Mahmoud";
+            emp.Dept = "IT";
+            Console.WriteLine(EmpDetails(emp)); //1 Mahmoud IT
         }
         static string SayHello()
         {
@@ -24,6 +49,44 @@ namespace ConsoleApp1
         {
             string fullname = name + " Family Name";
             return fullname;
+        }
+
+        static string SayHello(string fName, string lName)
+        {
+            string fullname = fName + " " + lName;
+            return fullname;
+        }
+        static string SayHello(Employee emp)
+        {
+            return emp.FullName;
+        }
+
+        static Employee GetDetails(Employee emp)
+        {
+            return emp;
+        }
+
+        static string EmpDetails(Employee obj)
+        {
+            return obj.ID + " " + obj.FullName+" " + obj.Dept;
+        }
+      
+
+    }
+
+    class Employee
+    {
+        public int ID
+        {
+            get; set;
+        }
+        public string FullName
+        {
+            get; set;
+        }
+        public string Dept
+        {
+            get; set;
         }
     }
 }
